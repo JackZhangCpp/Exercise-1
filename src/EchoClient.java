d959685c269feab3a00b57666ac6a938b545eedf
Jack Zhang <newb321@yahoo.com>
2017-09-28 03:13:45 -0700

    Second Part

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.io.OutputStream;
 public final class EchoClient {
 
--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,37 +1,65 @@
 
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Random;
+import java.io.IOException;
+
 public final class EchoServer {
 
     public static void main(String[] args) throws Exception {
+    	
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                  
-                	String userStr;
-                    
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
-
-                    
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 
-                   
-                    while((userStr=in.readLine()) != null) {
-                    	out.println(userStr); 
+        	
+        	while (true) {
+        		
+                    Socket socket = serverSocket.accept();
+                    ClientHandler handler = new ClientHandler(socket);
+                    handler.start();
                     	
-                    }
-                }
-            }
+        	}
+                
         }
+        
     }
 }
+
+class ClientHandler extends Thread{
+	Socket socket;
+	ClientHandler(Socket socket)
+	{
+		this.socket = socket;
+	
+}
+public void run()
+{
+	try {
+	String userStr;
+    String address = socket.getInetAddress().getHostAddress();
+    System.out.printf("Client connected: %s%n", address);
+    OutputStream os = socket.getOutputStream();
+    PrintStream out = new PrintStream(os, true, "UTF-8");
+    out.printf("Hi %s, thanks for connecting!%n", address);
+
+    
+    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+ 
+   
+    while((userStr=in.readLine()) != null) {
+    	if(userStr.equals("exit"))
+    	{
+    		System.out.printf("Client disconnected: %s%n", address);
+			socket.close();
+			break;
+		}
+    	out.println(userStr); 
+    	
+    	
+    }
+	}catch(Exception e){return;}
+}
+}
+	
