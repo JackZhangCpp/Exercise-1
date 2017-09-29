9fcc94275c9b13e8667cb2b540713d61332fdfa1
Jack Zhang <newb321@yahoo.com>
2017-09-28 18:35:00 -0700

    Implemented Multiple simultaneous Clients

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -6,35 +6,35 @@ import java.io.PrintStream;
 import java.io.OutputStream;
 public final class EchoClient {
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
         try (Socket socket = new Socket("localhost", 22222)) {
         
-         	String str;
-         	
+          String str;
+          
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             System.out.println(br.readLine());
-          
+            
             OutputStream os = socket.getOutputStream();
             PrintStream out = new PrintStream(os, true, "UTF-8");
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        	System.out.print("<Client>: "); 
+         System.out.print("<Client>: "); 
             BufferedReader svin= new BufferedReader(new InputStreamReader(System.in));
             
             
             while((str=svin.readLine())!=null) {
-            	out.println(str);
-            	System.out.println("<Server> " + in.readLine());
-            	System.out.print("<Client>: "); 
-
+             out.println(str);
+             if(str.equals("exit"))
+             {break;}
+             else{
+             System.out.println("<Server> " + in.readLine());
+             System.out.print("<Client>: "); 
+             }
             }
             
         }
     }
 }
-
-
-
 
 
--- src/EchoServer.java
+++ src/EchoServer.java
@@ -8,57 +8,53 @@ import java.io.InputStreamReader;
 
 
 public final class EchoServer {
-
-    public static void main(String[] args) throws Exception {
-    	
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
-        	
-        	while (true) {
-        		
-                    Socket socket = serverSocket.accept();
-                    ClientHandler handler = new ClientHandler(socket);
-                    handler.start();
-                    	
-        	}
-                
-        }
-        
-    }
-}
-
-class ClientHandler extends Thread{
-	Socket socket;
-	ClientHandler(Socket socket)
-	{
-		this.socket = socket;
-	
-}
-public void run()
-{
-	try {
-	String userStr;
-    String address = socket.getInetAddress().getHostAddress();
-    System.out.printf("Client connected: %s%n", address);
-    OutputStream os = socket.getOutputStream();
-    PrintStream out = new PrintStream(os, true, "UTF-8");
-    out.printf("Hi %s, thanks for connecting!%n", address);
-
-    
-    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
- 
-   
-    while((userStr=in.readLine()) != null) {
-    	if(userStr.equals("exit"))
-    	{
-    		System.out.printf("Client disconnected: %s%n", address);
-			socket.close();
-			break;
-		}
-    	out.println(userStr); 
-    	
-    	
-    }
-	}catch(Exception e){return;}
-}
-}
+	 private static Socket ssocket;
+	    public static void main(String[] args) throws Exception {
+	        try (ServerSocket serverSocket = new ServerSocket(22222)) {
+	         while (true) {
+	          Runnable Cli = () ->{
+	           
+	           try {
+	           String userStr;
+	           Socket socket = ssocket;
+	              String address = socket.getInetAddress().getHostAddress();
+	              System.out.printf("Client connected: %s%n", address);
+	             
+	              OutputStream os = socket.getOutputStream();
+	              PrintStream out = new PrintStream(os, true, "UTF-8");
+	              out.printf("Hi %s, thanks for connecting!%n", address);   
+	           
+	              BufferedReader in = new BufferedReader(new InputStreamReader (socket.getInputStream()));
+	           
+	              while((userStr=in.readLine()) != null) {
+	               if(userStr.equals("exit"))
+	               {
+	                System.out.printf("Client disconnected: %s%n", address);
+	             socket.close();
+	             userStr=null;
+	            }
+	               else
+	                {
+	                 out.println(userStr); 
+	                }
+	               
+	              }
+	           }catch(Exception e){return;}
+	      
+	          };
+	                    
+	           try
+	           {
+	           Socket socket= serverSocket.accept();
+	           EchoServer.ssocket = socket;
+	                    Thread newCli = new Thread(Cli);
+	                    newCli.start();
+	                    
+	           }catch(Exception e) {return;}  
+	         }
+	                
+	        }
+	        
+	    }
+	}
 	
